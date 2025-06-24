b42228ccb2190a7405d1eb2bbd411fc11fc411f7
arabelacarceles <146352778+arabelacarceles@users.noreply.github.com>
2025-06-24 18:50:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 import streamlit as st
 
-st.set_page_config(page_title="AXA Tech Graduate Program", layout="centered")
+st.set_page_config(page_title="Inicio", layout="centered")
 
-st.image("https://raw.githubusercontent.com/arabelacarceles/axapanel/main/images/logo.png", width=100)
-st.markdown("<h1 style='text-align: center; color: white; background-color: #00008B;'>TECH GRADUATE PROGRAM</h1>", unsafe_allow_html=True)
+# Fondo azul usando componentes de Streamlit (con st.markdown seguro)
+st.markdown(
+    """
+    <style>
+    [data-testid="stAppViewContainer"] {
+        background-color: #00008B;
+    }
+    h1 {
+        text-align: center;
+        color: white;
+        font-size: 2.5em;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Espaciado arriba
+st.markdown("###")
+st.markdown("###")
+
+# Mostrar imagen (ajusta el path y tamaño)
+st.image("images/logo.png", width=300)
+
+# Título
+st.markdown("<h1>TECH GRADUATE PROGRAM</h1>", unsafe_allow_html=True)
