6ccc09deaddfc86230d801e2ca822a8965b30352
arabelacarceles <146352778+arabelacarceles@users.noreply.github.com>
2025-06-24 17:31:55 -0400

    change image

--- presentation.py
+++ presentation.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Inicio", layout="centered")
 
-# Fondo azul oscuro y estilo general
+# Fondo azul con estilo
 st.markdown(
     """
     <style>
@@ -15,13 +15,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Página centrada con logo e introducción
-st.markdown(
-    """
-    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
-        <img src="images/logo.png" style="width: 300px;">
-        <h1 style="color: white; margin-top: 20px;">TECH GRADUATE PROGRAM</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# Mostrar imagen con st.image()
+st.markdown("<br><br><br>", unsafe_allow_html=True)
+st.image("images/logo.png", width=300)
+st.markdown("<h1 style='text-align: center; color: white;'>TECH GRADUATE PROGRAM</h1>", unsafe_allow_html=True)
