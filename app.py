276d07167f535cd4491b07a670ed61383589a7c4
arabelacarceles <146352778+arabelacarceles@users.noreply.github.com>
2025-06-24 18:36:21 -0400

    begin

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="AXA Tech Graduate Program",
-    layout="wide"
-)
+st.set_page_config(page_title="AXA Tech Graduate Program", layout="centered")
 
-logo_url = "https://raw.githubusercontent.com/arabelacarceles/axapanel/main/images/logo.png"
-
-# Estilo para fondo y centrado
-st.markdown("""
-    <style>
-        html, body, [class*="css"] {
-            background-color: #00008B !important;
-            height: 100%;
-            margin: 0;
-        }
-        .container {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .title {
-            color: white;
-            font-size: 28px;
-            font-weight: bold;
-            margin-top: 20px;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# HTML+Streamlit render
-st.markdown('<div class="container">', unsafe_allow_html=True)
-st.image(logo_url, width=100)
-st.markdown('<div class="title">TECH GRADUATE PROGRAM</div>', unsafe_allow_html=True)
-st.markdown('</div>', unsafe_allow_html=True)
+st.image("https://raw.githubusercontent.com/arabelacarceles/axapanel/main/images/logo.png", width=100)
+st.markdown("<h1 style='text-align: center; color: white; background-color: #00008B;'>TECH GRADUATE PROGRAM</h1>", unsafe_allow_html=True)
